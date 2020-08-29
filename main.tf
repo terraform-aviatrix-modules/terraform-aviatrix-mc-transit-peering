041f4c22eda709759fa25a8479d07cd4a295b021
Dennizz <root@ipaddr.nl>
2020-08-29 18:02:27 +0200

    flatten

--- main.tf
+++ main.tf
@@ -1,5 +1,5 @@
 locals {
-  peerings = [
+  peerings = flatten([
     for gw in var.transit_gateways : [
       #list_selection = slice(var.transit_gateways, index(var.transit_gateways, gw) + 1, length(var.transit_gateways) + 1) #Should result in list of remaining gateways
       for peer_gw in slice(var.transit_gateways, index(var.transit_gateways, gw) + 1, length(var.transit_gateways)) : {
@@ -7,7 +7,7 @@ locals {
         gw2 = peer_gw
       }
     ]
-  ]
+  ])
 }
 
 resource "aviatrix_transit_gateway_peering" "test_transit_gateway_peering" {
