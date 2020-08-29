1d8e84da82c41926583953b79bab117f0e16f61c
Dennizz <root@ipaddr.nl>
2020-08-29 17:55:32 +0200

    add resource

--- main.tf
+++ main.tf
@@ -2,18 +2,16 @@ locals {
   peerings = [
     for gw in var.transit_gateways : [
       #list_selection = slice(var.transit_gateways, index(var.transit_gateways, gw) + 1, length(var.transit_gateways) + 1) #Should result in list of remaining gateways
-      for item in slice(var.transit_gateways, index(var.transit_gateways, gw) + 1, length(var.transit_gateways)) : {
+      for peer_gw in slice(var.transit_gateways, index(var.transit_gateways, gw) + 1, length(var.transit_gateways)) : {
         gw1 = gw
-        gw2 = item
+        gw2 = peer_gw
       }
     ]
   ]
 }
 
-/*
 resource "aviatrix_transit_gateway_peering" "test_transit_gateway_peering" {
-  for_each 
-  transit_gateway_name1             = "transit-Gw1"
-  transit_gateway_name2             = "transit-Gw2"
+  for_each = local.peerings
+  transit_gateway_name1             = each.value.gw1
+  transit_gateway_name2             = each.value.gw2
 }
-*/
